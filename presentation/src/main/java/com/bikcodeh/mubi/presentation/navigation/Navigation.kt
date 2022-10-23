063be65426e528fb5fcf4028104ca84be793d02f
Bikcode <bikcodeh@gmail.com>
2022-10-22 22:57:49 -0500

    create nav host and implements splash screen with its navigation

--- presentation/src/main/java/com/bikcodeh/mubi/presentation/navigation/Navigation.kt
+++ presentation/src/main/java/com/bikcodeh/mubi/presentation/navigation/Navigation.kt
@@ -1,8 +1,31 @@
 package com.bikcodeh.mubi.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.bikcodeh.mubi.presentation.screens.home.HomeScreen
+import com.bikcodeh.mubi.presentation.screens.splash.SplashScreen
 
 @Composable
-fun MubiNavigation() {
-    
+fun MubiNavigation(
+    navController: NavHostController
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.Splash.route
+    ) {
+        composable(route = Screens.Splash.route) {
+            SplashScreen(navigateToHome = {
+                navController.navigate(Screens.Home.route) {
+                    popUpTo(Screens.Splash.route) {
+                        inclusive = true
+                    }
+                }
+            })
+        }
+        composable(route = Screens.Home.route) {
+            HomeScreen(onClickItem = {})
+        }
+    }
 }
