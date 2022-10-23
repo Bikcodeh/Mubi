47ee2dc1c5849ab122bd6423cf0eb3028cbcd788
Bikcode <bikcodeh@gmail.com>
2022-10-23 02:03:41 -0500

    create profile UI

---
+++ presentation/src/main/java/com/bikcodeh/mubi/presentation/components/GenericTopBar.kt
@@ -0,0 +1,70 @@
+package com.bikcodeh.mubi.presentation.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.constraintlayout.compose.ConstraintLayout
+import com.bikcodeh.mubi.presentation.R
+import com.bikcodeh.mubi.presentation.theme.DEFAULT_HEIGHT_TOP_BAR
+import com.bikcodeh.mubi.presentation.theme.GhostWhite
+import com.bikcodeh.mubi.presentation.theme.PADDING_32
+import com.bikcodeh.mubi.presentation.theme.VeryLightBlue
+
+@Composable
+fun GenericTopBar(
+    onBack: () -> Unit,
+    @StringRes titleResId: Int
+) {
+    Surface(
+        color = VeryLightBlue,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(DEFAULT_HEIGHT_TOP_BAR)
+    ) {
+        ConstraintLayout(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            val (back, title) = createRefs()
+            IconButton(
+                onClick = onBack,
+                modifier = Modifier.constrainAs(back) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                }) {
+                Icon(
+                    Icons.Default.ArrowBack,
+                    contentDescription = stringResource(id = R.string.back),
+                    tint = GhostWhite
+                )
+            }
+            Text(
+                text = stringResource(id = titleResId),
+                color = GhostWhite,
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier.constrainAs(title) {
+                    start.linkTo(back.end, margin = PADDING_32)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                })
+        }
+    }
+}
+
+@Preview
+@Composable
+fun GenericTopBarPreview() {
+    GenericTopBar(onBack = {}, titleResId = R.string.mubi_title)
+}
---
+++ presentation/src/main/java/com/bikcodeh/mubi/presentation/screens/profile/ProfileScreen.kt
@@ -0,0 +1,8 @@
+package com.bikcodeh.mubi.presentation.screens.profile
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ProfileScreen() {
+
+}
