231cfce7a8c9d11a472876f2a8648d0790d09f84
Bikcode <bikcodeh@gmail.com>
2022-11-02 12:08:14 -0500

    implements unit testing for searchviewmodel

---
+++ presentation/src/test/java/com/bikcodeh/mubi/presentation/screens/search/SearchViewModelTest.kt
@@ -0,0 +1,25 @@
+package com.bikcodeh.mubi.presentation.screens.search
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class SearchViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @Test
+    fun getSearchTvShowsState() {
+    }
+
+    @Test
+    fun searchTvShows() {
+    }
+
+    @Test
+    fun searchTvShowsRemote() {
+    }
+}
